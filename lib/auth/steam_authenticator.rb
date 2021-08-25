a0f7b2dde606c19e67b8b8b2db8f49a782321179
Dmitry Antonov <chelog@octothorp.team>
2021-08-25 18:08:21 +0300

    display steamid instead of nickname in description

--- lib/auth/steam_authenticator.rb
+++ lib/auth/steam_authenticator.rb
@@ -13,6 +13,13 @@ class Auth::SteamAuthenticator < ::Auth::ManagedAuthenticator
     SiteSetting.steam_allow_revoke
   end
 
+  def description_for_auth_hash(auth_token)
+    return if auth_token&.info.nil?
+    info = auth_token.info
+    extra = auth_token.extra
+    extra["raw_info"]["steamid"] || info["nickname"]
+  end
+
   def register_middleware(omniauth)
     omniauth.provider :steam, setup: lambda { |env|
       strategy = env["omniauth.strategy"]
